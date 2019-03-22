c9f46cfb437a20aba413b4770c08ee3ff760c9fc
SolidRocketBooster <45150054+SolidRocketBooster@users.noreply.github.com>
2019-03-22 11:22:45 +0100

    First draft / not for use

---
+++ full_setup.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# Script installs light verson of software neede for work (in my opinion). It was created early in my DevOps career.
+# Save for server install. No upgrades
+apt update
+
+#Essential bins
+apr install vim curl net-tools iproute2 lsof
+
+#   -------------------------------
+#   1. ENVIRONMENT CONFIGURATION
+#   -------------------------------
+
+#   Change Prompt
+#   ------------------------------------------------------------
+    export GIT_PS1_SHOWDIRTYSTATE=true
+    export GIT_PS1_SHOWUNTRACKEDFILES=true
+    export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\] \[\033[33;1m\]\w\[\033[m\] (\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)) \$ "
+    export PS2="| => "
+
+
+#   Set Paths
+#   ------------------------------------------------------------
+export PATH="$PATH:/usr/local/bin/"
+
+#   Grep Collors
+#   ------------------------------------------------------------
+export GREP_OPTIONS=' — color=auto'
+
+#   Set Default Editor (change 'Vim' to the editor of your choice)
+#   ------------------------------------------------------------
+export EDITOR=/usr/bin/vim
+
+#   Set default blocksize for ls, df, du
+#   ------------------------------------------------------------
+export BLOCKSIZE=1k
+
+#   -----------------------------
+#   2. MAKE TERMINAL BETTER / ALIASES, QUICK SCRIPTS, ETC.
+#   -----------------------------
+
+alias mkdir='mkdir -pv'
+alias c='clear'
+alias q='exit'
+alias k='kill'
+alias q='exit'
+alias cs='clear;ls'
+alias vimrc='vim ~/.vimrc'
+alias bashrc='vim ~/.bash_profile'
+alias cd..='cd ../'
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ~='cd ~'
+mcd () { mkdir -p "$1" && cd "$1"; }
+
+#   lr:  Full Recursive Directory Listing
+#   ------------------------------------------
+alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
+
+#   mans:   Search manpage given in agument '1' for term given in argument '2' (case insensitive)
+#           displays paginated result with colored search terms and two lines surrounding each hit.            Example: mans mplayer codec
+#   --------------------------------------------------------------------
+    mans () {
+        man $1 | grep -iC2 --color=always $2 | less
+    }
+
+#   ---------------------------
+#   3. PROCESS MANAGEMENT
+#   ---------------------------
+
+#   findPid: find out the pid of a specified process
+#   -----------------------------------------------------
+#       Note that the command name can be specified via a regex
+#       E.g. findPid '/d$/' finds pids of all processes with names ending in 'd'
+#       Without the 'sudo' it will only find processes of the current user
+#   -----------------------------------------------------
+findPid () { lsof -t -c "$@" ; }
+
+#   topForever:  Continual 'top' listing (every 10 seconds)
+#   -----------------------------------------------------
+alias topForever='top -l 9999999 -s 10 -o cpu'
+
+#   ttop:  Recommended 'top' invocation to minimize resources
+#   ------------------------------------------------------------
+#       Taken from this macosxhints article
+#       http://www.macosxhints.com/article.php?story=20060816123853639
+#   ------------------------------------------------------------
+    alias ttop="top -R -F -s 10 -o rsize"
+
+#   my_ps: List processes owned by my user:
+#   ------------------------------------------------------------
+my_ps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
+
+#   ---------------------------
+#   4. NETWORKING
+#   ---------------------------
+
+alias myip='curl ip.appspot.com'
+
+#   ii:  display useful host related informaton
+#   -------------------------------------------------------------------
+    ii() {
+        echo -e "\nYou are logged on ${RED}$HOST"
+        echo -e "\nAdditionnal information:$NC " ; uname -a
+        echo -e "\n${RED}Users logged on:$NC " ; w -h
+        echo -e "\n${RED}Current date :$NC " ; date
+        echo -e "\n${RED}Machine stats :$NC " ; uptime
+        echo -e "\n${RED}Current network location :$NC " ; scselect
+        echo -e "\n${RED}Public facing IP Address :$NC " ;myip
+        #echo -e "\n${RED}DNS Configuration:$NC " ; scutil --dns
+        echo
+}
